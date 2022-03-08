51e9a00abf76a600969b73c25f1db67e0e6b1822
Khoaraoh <ngcaodangkhoa1@gmail.com>
2022-03-08 11:45:21 +0700

    add header layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Sidebar from './Component/Sidebar/Sidebar';
+import Header from './Component/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
       <Sidebar/>
+      <Header/>
     </div>
   );
 }
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header()
+{
+    return(
+        <div className='header'>
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default Header
