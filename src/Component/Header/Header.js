68b2db0b3eab972bc4fbd453ee17b5a98a1a3170
Khoaraoh <ngcaodangkhoa1@gmail.com>
2022-03-10 11:12:23 +0700

    finish header without logic

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,10 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 import './Header.css'
 
 function Header()
 {
     return(
         <div className='header'>
-            <p>Hello</p>
+            <FontAwesomeIcon className='icon' icon={faCoffee} />
+            <FontAwesomeIcon className='icon' icon={faCoffee} />
+            <div className='search'>
+                <FontAwesomeIcon className='searchIcon' icon={faCoffee} />
+                <input className='searchInput' placeholder='Nhập tên bài hát, nghệ sĩ hoặc MV...'></input>
+                <FontAwesomeIcon className='closeIcon' icon={faCoffee} />
+            </div>
+            <div className='rightButton'>
+                <button className='button'>
+                </button>
+                <button className='button'></button>
+                <button className='button'></button>
+                <button className='button'></button>
+                <button className='button'></button>
+            </div>
         </div>
     )
 }
